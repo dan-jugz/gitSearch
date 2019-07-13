3b1b25ca236e41a7aadfafa95b5ba70267ed368b
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-14 00:46:30 +0300

    declare and link the search service

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from 'src/app/services/search.service';
 
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers:[SearchService]
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private searchService:SearchService) { }
 
   ngOnInit() {
   }
