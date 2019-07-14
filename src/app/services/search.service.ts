9437e0eb2ae046391da39701136edfaa3196ecb8
daniel njuguna <njugunadaniel364@gmail.com>
2019-07-15 02:16:11 +0300

    added the source modules to the app module

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
-
-  constructor() { }
+searchUser:string;
+ githubUser:any;
+// apiUrl = environment.apiUrl;
+// access_token= environment.access_token;
+  constructor(private http:HttpClien) { }
 }
